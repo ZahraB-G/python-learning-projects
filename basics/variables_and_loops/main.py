e1c4b79fc9d1bf367aba65f0a6e577019a183a56
Zahra B-G <zahraginns@gmail.com>
2025-10-15 11:04:25 -0500

    setup python project

--- basics/variables_and_loops/main.py
+++ basics/variables_and_loops/main.py
@@ -1,1 +1,6 @@
-print('Hello World')
+# Simple calculator for two numbers
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+sum_result = num1 + num2 
+diff_result = num1 - num2
+print(f'sum: {sum_result}, difference: {diff_result}')
