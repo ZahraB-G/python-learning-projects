8929b73202f65788b5c578b1832921a4c9432e2f
Zahra B-G <zahraginns@gmail.com>
2025-10-31 15:30:41 -0500

    update file

---
+++ advanced/pandas/csv_file_handling.py
@@ -0,0 +1,11 @@
+import pandas as pd
+richest = pd.read_csv('advanced/pandas/top_100_richest.csv',usecols=['name'])
+print(richest.dtypes)
+# print(richest)
+print(richest.head()) # returns the first 5 rows by default
+print('==================================================================================')
+print(richest.head(10)) # returns the first 10 rows
+print('==================================================================================')
+print(richest.tail(10))
+print('==================================================================================')
+print(richest.sort_values('name'))
--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-nums = [3,3]
-target = 6
-result = []
-for i in range(len(nums)):
-    goal = target - nums[i]
-    if goal in nums[i+1:]:
-        result.append(nums.index(goal,i+1))
-        result.append(i)
-
-result.sort()     
-print(result)
+import pandas as pd
+numbers = [1,2,3,4,5]
+print(pd.Series(numbers).dtype)
