1fc2a052d9b4b74e7fcdf6502fca116c35167444
Zahra B-G <zahraginns@gmail.com>
2025-11-04 15:17:26 -0600

    update file

---
+++ advanced/pandas/dataframe_practice_3.py
@@ -0,0 +1,8 @@
+import pandas as pd
+cols = ['Series_Title','IMDB_Rating','No_of_Votes']
+movies = pd.read_csv('advanced/pandas/imdb_top_1000.csv',usecols=cols)
+movies = pd.DataFrame(data=movies)
+print('The top 10 Movies based on rating\n',movies.nlargest(n=10,columns=['IMDB_Rating']))
+print('==================================================================================')
+print('The bottom 10 Movies based on rating\n',movies.nsmallest(n=10,columns=['IMDB_Rating']))
+#print(movies)
---
+++ advanced/pandas/dataframe_practice_4.py
@@ -0,0 +1,7 @@
+import pandas as pd
+movies = pd.read_csv('advanced/pandas/imdb_top_1000.csv').head(3)
+movies.drop(columns=['Poster_Link','Certificate','Overview','Runtime'],inplace=True)
+
+print('Remove the three columns from the dataframe\n',movies)
+print('==================================================================================')
+print(movies.drop([1,2]))
