e97e2c2a376e4c1ec42ffe08743ec753b93099fb
Zahra B-G <zahraginns@gmail.com>
2025-11-03 23:08:00 -0600

    update file

---
+++ advanced/pandas/dataframe_1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+richest = pd.read_csv('advanced/pandas/top_100_richest.csv')
+print('The Shape of richest is ',richest.shape)
+print('The number of elements in the dataframe is ',richest.size)
+print('The index of the dataframe is ', richest.index)
+print('The coloumns of the dataframe is ', richest.columns)
---
+++ advanced/pandas/dataframe_2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+cols = ['Series_Title','IMDB_Rating', 'No_of_Votes','Gross','Released_Year','Genre']
+movies = pd.read_csv('advanced/pandas/imdb_top_1000.csv',usecols=cols)
+print('info() method gives a lot of information such as ',movies.info())
+# Convert Grocc into an integer so it can be as numric data
+movies['Gross'] = movies['Gross'].str.replace(',','')
+movies['Gross'].fillna(0,inplace=True)
+movies['Gross']=movies['Gross'].astype('int64')
+
+print(movies.count())
