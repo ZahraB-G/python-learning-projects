c2ddddeee5cc51b2a3465d9ee2061afe3ca28e52
Zahra B-G <zahraginns@gmail.com>
2025-11-05 22:52:49 -0600

    update file

--- data_science_basics/pandas/dataframe_introduction.py
+++ data_science_basics/pandas/dataframe_introduction.py
@@ -3,5 +3,19 @@
 import pandas as pd
 # Create a dataframe
 employees = {'Name':['Austin','Jen','Tom'],'Age':[25, 43, 56]}
-df = pd.DataFrame(data=employees)
-print(df)
+df = pd.DataFrame(data=employees, index=['Employee 1','Employee 2','Employee 3'])
+print('Prnt the data\n',df)
+print('===========================================')
+# Accessing data in dataframe
+print('Access the information for the Employee 1\n',df.loc['Employee 1'])
+print('Access the information for the Employee 2\n',df.iloc[2])
+print('===========================================')
+# Adding new column
+df['Job'] = ['Consultant','Designer','Accountant']
+print('Print the new Dataframe\n',df)
+print('===========================================')
+# Adding new rows
+new_df = pd.DataFrame(data = [{'Name':'Sandy','Age':'30','Job':'Developer'}], index=['Employee 4'])
+df = pd.concat([df,new_df])
+print('Print the new Dataframe\n',df)
+print('===========================================')
