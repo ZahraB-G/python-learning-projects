8161fcb21edd4f976680f3d8b2097a12de5ff48b
Zahra B-G <zahraginns@gmail.com>
2025-11-10 11:37:43 -0600

    update file

--- data_science_basics/matplot/introduction.py
+++ data_science_basics/matplot/introduction.py
@@ -5,6 +5,7 @@ grade = [89,100,76,50]
 plt.plot(subject,grade,marker='*',markersize=10,markerfacecolor='red')
 plt.show()
 ################################################
+# Ploting using Numpy array since they are faster than python lists
 x = np.array([1,2,3,4,5])
 y = np.array([100,28,34,67,23])
 plt.plot(x,y,marker='.'
@@ -16,3 +17,34 @@ plt.plot(x,y,marker='.'
          ,color='darkblue'
          )
 plt.show()
+################################################
+# Ploting using the same style
+line_style = dict(marker='.'
+         ,markersize=30
+         ,markerfacecolor='yellow'
+         ,markeredgecolor='red'
+         ,linestyle='dashed'
+         ,linewidth=3
+         ,color='darkblue')
+z = np.array([76,2,33,45,67])
+plt.plot(x,z,**line_style)
+plt.show()
+################################################
+# Adding title and naming x-axis and y-axis
+plt.title('Class Size',fontsize=20, fontfamily='Arial', fontweight='bold', color='red')
+plt.xlabel('Year',fontsize=20,fontfamily='Arial',fontweight='bold',color='blue')
+plt.ylabel('Students',fontsize=20,fontfamily='Arial',fontweight='bold',color='blue')
+plt.plot(x,z,**line_style)
+plt.show()
+################################################
+# gride() = Helps make plots easier to read by adding reference lines.
+x = [1,2,3,4,5]
+y = [5,10,15,20,25]
+plt.grid(axis='y',
+         linewidth=2,
+         color='gold',
+         )
+plt.plot(x,y)
+plt.show()
+################################################
+# Bar Chart = compare categories of data by reperesenting each categor with a bar
